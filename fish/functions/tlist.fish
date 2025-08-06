8fd6d896da71bd84246df4c5bfa831849e8c0fa4
Vincent Rischmann <vincent@rischmann.fr>
2025-08-06 16:41:07 +0200

    update tlist to print with colors

--- fish/functions/tlist.fish
+++ fish/functions/tlist.fish
@@ -1,4 +1,4 @@
 function tlist --description "List incomplete tasks only"
     set file (__tasks_get_file $argv[1])
-    tasks --file $file ls | grep "\[ \]"
+    tasks --color=always --file $file ls | grep "\[ \]"
 end
